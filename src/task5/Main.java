11b21e4eb7afed86dae765177f4596c2ae03aafa
Katsiaryna Lasitsa <katya.lasicza.03@mail.ru>
2022-12-26 18:50:05 +0300

    final task 5

--- src/task5/Main.java
+++ src/task5/Main.java
@@ -1,8 +1,13 @@
 package task5;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String mainString = "hello";
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter the word: ");
+        String mainString = input.nextLine();
+        
         StringMethod.lettersDuplicate(mainString);
     }
 }
--- src/task5/StringMethod.java
+++ src/task5/StringMethod.java
@@ -2,18 +2,13 @@ package task5;
 
 public class StringMethod {
     public static void lettersDuplicate(String mainString) {
-        String fi = "hello";
-        String s1 = "";
-        String str = "";
-        for (int i = 0; i < fi.length(); i++) {
-            char s = mainString.charAt(i);
-            s1 = String.valueOf(s);
+        char[] lettersArray = mainString.toCharArray();
+        StringBuilder sb = new StringBuilder();
 
+        for(char letters : lettersArray){
+            sb.append(letters).append(letters);
         }
-        str = s1.concat(s1);
-        String f = mainString.replace(s1, str);
-        fi = f.concat(f);
-        System.out.println(fi);
 
+        System.out.println(sb);
     }
 }
